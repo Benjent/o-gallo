dde3743e79eb5e55be84acc391a314f683fe0e69
Benjent <Benjent@hotmail.fr>
2024-10-02 16:15:55 +0200

    feat: retrieve repo from piksail org

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				banner: "url('/src/assets/images/banner.jpg')",
+			},
+			colors: {
+				primary: "#ed7681",
+				agro: {
+					dark: "#ece58d",
+					light: "#f7f4d5",
+				},
+			}
+		},
+	},
+	plugins: [],
+}
