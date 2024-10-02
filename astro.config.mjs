c759639e70b8e2c3471181a916f76e32e228fa80
Benjent <Benjent@hotmail.fr>
2024-10-02 16:22:39 +0200

    feat: autodeploy on gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://benjent.github.io/o-gallo/',
+  base: '/o-gallo',
   integrations: [tailwind()]
 });
